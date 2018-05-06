71763295e2016f83645aff20ba347cb9bbce7abf
rajesh212 <rajeshonl9@gmail.com>
2018-05-06 12:35:15 +0000

    sftp_tomcat.sh edited online with Bitbucket

--- sftp_tomcat.sh
+++ sftp_tomcat.sh
@@ -1,7 +1,7 @@
 mvn target/myweb*.war target/myweb.war
 sftp ec2-user@35.178.160.225  <<EOF
 rename /opt/tomcat/webapps/myweb.war /tmp/myweb.war.back
-rm /opt/tomcat/webapps/myweb*
+rm -r /opt/tomcat/webapps/myweb*
 put target/myweb.war /opt/tomcat/webapps/
 bye
 EOF
