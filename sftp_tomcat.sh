499d482071601459bc5cc20e83070ef02905f06e
rajesh212 <rajeshonl9@gmail.com>
2018-05-06 12:30:45 +0000

    sftp_tomcat.sh edited online with Bitbucket

--- sftp_tomcat.sh
+++ sftp_tomcat.sh
@@ -1,11 +1,8 @@
-mv target/myweb*.war target/myweb.war
-
-sftp ec2-user@172.31.29.38 <<EOF
-
+mvn target/myweb*.war target/myweb.war
+sftp ec2-user@35.178.160.225
 rename /opt/tomcat/webapps/myweb.war /tmp/myweb.war.back
 rm /opt/tomcat/webapps/myweb*
-put target/myweb.war  /opt/tomcat/webapps/
+put target/myweb.war /opt/tomcat/webapps/
 bye
-EOF
-ssh ec2-user@172.31.29.38 /opt/tomcat/bin/shutdown.sh
-ssh ec2-user@172.31.29.38 /opt/tomcat/bin/startup.sh
+ssh ec2-user@35.178.160.225 /opt/tomcat/bin/.shutdown.sh
+ssh ec2-user@35.178.160.225 /opt/tomcat/bin/.startup.sh
